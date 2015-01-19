1f906bb9305734a42c91d1709ce95feafaa50083
Jonathan Tribouharet <jonathan@siu-soon.com>
2015-01-19 15:49:53 +0100

    Initial Commit

---
+++ Supinfo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Supinfo
+//
+//  Created by Jonathan on 19/01/15.
+//  Copyright (c) 2015 Jonathan Tribouharet. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Supinfo/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Supinfo
+//
+//  Created by Jonathan on 19/01/15.
+//  Copyright (c) 2015 Jonathan Tribouharet. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
