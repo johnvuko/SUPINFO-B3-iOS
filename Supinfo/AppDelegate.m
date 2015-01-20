256cf9100eae158d6af9e92f631bedd081b90f8d
Jonathan Tribouharet <jonathan@siu-soon.com>
2015-01-20 14:45:00 +0100

    3.2.2. The Navigation Controller

--- Supinfo/AppDelegate.m
+++ Supinfo/AppDelegate.m
@@ -22,7 +22,10 @@
     self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
     [self.window makeKeyAndVisible];
 
-    self.window.rootViewController = [WelcomeTableViewController new];
+    WelcomeTableViewController *controller = [WelcomeTableViewController new];
+    UINavigationController *navController = [[UINavigationController alloc] initWithRootViewController:controller];
+    
+    self.window.rootViewController = navController;
     
     return YES;
 }
